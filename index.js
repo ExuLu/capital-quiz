433519366f9cfe69908d081efe3f7e66713f6802
Alena Sharai <exultantislupus@gmail.com>
2026-01-30 15:39:41 +0000

    feat: add db query

--- index.js
+++ index.js
@@ -18,11 +18,16 @@ const db = new pg.Client({
 
 db.connect();
 
-let quiz = [
-  { country: 'France', capital: 'Paris' },
-  { country: 'United Kingdom', capital: 'London' },
-  { country: 'United States of America', capital: 'New York' },
-];
+let quiz;
+
+db.query('SELECT * FROM capitals', (err, res) => {
+  if (err) {
+    console.error('💥 Error executing query', err.message);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
 
 let totalCorrect = 0;
 
